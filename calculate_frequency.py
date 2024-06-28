1f1be1ae8622b7afa33928d85ad3d62964f8a128
haibaraaaaai <dennyxudp0402@gmail.com>
2024-06-28 13:26:20 +0100

    bug fixes

--- calculate_frequency.py
+++ calculate_frequency.py
@@ -31,25 +31,23 @@ def process_frame(frame, roi_size=64):
     if selected_contour is None:
         return None, None
 
-    leftmost = tuple(selected_contour[selected_contour[:, :, 0].argmin()][0])
-    rightmost = tuple(selected_contour[selected_contour[:, :, 0].argmax()][0])
-    
-    if np.linalg.norm(
-        np.array(leftmost) - np.array(center)) > np.linalg.norm(
-            np.array(rightmost) - np.array(center)):
-        farthest_point = leftmost
-    else:
-        farthest_point = rightmost
+    max_distance = 0
+    furthest_point = None
+    for point in selected_contour:
+        point = point[0]
+        distance = np.linalg.norm(np.array(point) - np.array(center))
+        if distance > max_distance:
+            max_distance = distance
+            furthest_point = point
 
-    return selected_contour, farthest_point
+    return selected_contour, tuple(furthest_point)
 
 def calculate_freq(prev_point, curr_point):
     direction = np.arctan2(
         curr_point[1] - prev_point[1], curr_point[0] - prev_point[0])
     return direction
 
-def calculate_frequency(
-        video_path, roi_size=64, frame_interval=20, output_fps=None):
+def calculate_frequency(video_path, roi_size=64, frame_interval=20, output_fps=None):
     cap = cv2.VideoCapture(video_path)
     original_fps = int(cap.get(cv2.CAP_PROP_FPS))
     fps = output_fps if output_fps else original_fps
@@ -57,11 +55,8 @@ def calculate_frequency(
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     video_name = os.path.splitext(os.path.basename(video_path))[0]
-    output_video_path = os.path.join(
-        os.path.dirname(video_path), f"{video_name}_tracked.avi")
-    out = cv2.VideoWriter(
-        output_video_path, cv2.VideoWriter_fourcc(*'XVID'),
-          fps, (frame_width, frame_height))
+    output_video_path = os.path.join(os.path.dirname(video_path), f"{video_name}_tracked.avi")
+    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'XVID'), fps, (frame_width, frame_height))
 
     frame_count = 0
     prev_point = None
@@ -75,9 +70,9 @@ def calculate_frequency(
             out.release()
             return []
 
-        contour, farthest_point = process_frame(frame, roi_size)
-        if farthest_point is not None:
-            prev_point = farthest_point
+        selected_contour, furthest_point = process_frame(frame, roi_size)
+        if furthest_point is not None:
+            prev_point = furthest_point
             break
 
     if prev_point is None:
@@ -94,18 +89,18 @@ def calculate_frequency(
         if not ret:
             break
 
-        contour, farthest_point = process_frame(frame, roi_size)
-        if farthest_point is not None:
-            direction = calculate_freq(prev_point, farthest_point)
+        selected_contour, furthest_point = process_frame(frame, roi_size)
+        if furthest_point is not None:
+            direction = calculate_freq(prev_point, furthest_point)
             
             total_angle_change += direction
             frequencies.append(direction / (2 * math.pi) * fps)
 
-            prev_point = farthest_point
+            prev_point = furthest_point
 
-            if contour is not None:
-                cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-            cv2.circle(frame, farthest_point, 5, (0, 0, 255), -1)
+            if selected_contour is not None:
+                cv2.drawContours(frame, [selected_contour], -1, (0, 255, 0), 2)
+            cv2.circle(frame, furthest_point, 5, (0, 0, 255), -1)
 
         out.write(frame)
         frame_count += 1
@@ -124,8 +119,7 @@ def calculate_frequency(
     cap.release()
     out.release()
 
-    frequency_file_path = os.path.join(
-        os.path.dirname(video_path), f"{video_name}_frequencies.txt")
+    frequency_file_path = os.path.join(os.path.dirname(video_path), f"{video_name}_frequencies.txt")
     with open(frequency_file_path, 'w') as f:
         for freq in frequencies:
             f.write(f"{freq}\n")
